c5032b289741a8abc7fd57bf7c7058310358c2f6
Dali918 <dalitbanda@gmail.com>
2023-02-23 23:46:26 -0500

    fixed display content program

--- lab2/q1b_create_open_file.c
+++ lab2/q1b_create_open_file.c
@@ -1,20 +1,24 @@
-#include <fcntl.h>
-#include <errno.h>
 #include <unistd.h>
-
-
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
 int main(int argc, char *argv[])
 {
     int fd;
+    //argument testing
     if (argc != 2)
     {
         printf("File parameter is missing\n");
         return 1;
     }
-    char *filepath = argv[1];
     errno = 0;
+    char *filepath = argv[1];
+    //create and error check
     fd = open(filepath, O_CREAT | O_RDWR);
-    if (fd == -1)
+    if (fd < 0)
     {
         printf("\n open () failed with error [%s]\n", strerror(errno));
         return 1;
@@ -26,16 +30,14 @@ int main(int argc, char *argv[])
         since we opened it in read-only mode. Also once done with processing,
         file needs to be close */
     }
-
+    //  Test for existence 
     int returnval = access(filepath, F_OK);
     if (returnval == 0)
         printf("\n %s exists\n", filepath);
-    else
-    {
+    else{
         printf("\n %s does not exist and was not created\n", filepath);
     }
-
+    // close file descriptor
     close(fd);
-
     return 0;
 }
---
+++ lab2/q2_displaycontent.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+
+const int buff_size = 2048;
+
+int main(int argc, char *argv[])
+{
+    char *filepath = argv[1];
+    int exists, readaccess, fd;
+    exists = access(filepath, F_OK);
+    readaccess = access(filepath, R_OK);
+    // error check file existence and access
+    if (exists || readaccess || argc!=2)
+    {
+        if(argc!=2)
+            printf("not enough or too many argments\n");
+        else if (errno == ENOENT)
+            printf("%s does not exist\n", filepath);
+        else if (errno == EACCES)
+            printf("%s is not accessible\n", filepath);
+        return 0;
+    }
+    //open the file and errorcheck
+    fd = open(filepath, O_RDWR);
+    if (fd < 0)
+    {
+        perror("error open file");
+        return 1;
+    }
+    // number of bytes read
+    ssize_t char_read;
+    char buff[buff_size];
+    // read and error check
+    char_read = read(fd, buff, buff_size);
+    if (char_read < 0)
+    {
+        perror("error reading file");
+        return 1;
+    }
+    // print to terminal
+    printf("%s",buff);
+    //close file descriptor
+    close(fd);
+    return 0;
+}
