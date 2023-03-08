b7a5b35c0349017422acabb21cc8decf8e8bd8fc
Dali918 <dalitbanda@gmail.com>
2023-03-08 16:05:23 -0500

    q4 parent process complete with spawned children

--- lab3/q4_parent_prcs.c
+++ lab3/q4_parent_prcs.c
@@ -25,14 +25,15 @@ int main(int argc, char *argv[])
         printf("\nchild process 1, pid: %d\n", getpid());
 
         // compile program
-        if (system("gcc -o q4_prcs_p1 q4_prcs_p1.c") == -1)
+        if (system("gcc -o child q4_prcs_p1.c") == -1)
         {
-            perror("system");
+            perror("system compile error");
             exit(EXIT_FAILURE);
         }
-        // execute binary to create files
-        char *path = "./q4_prcs_p1";
-        char *args[] = {"q4_prcs_p1", NULL};
+        // execute program with args using execv
+        char *path = "./child";
+        char *args[] = {"child", NULL};
+        //error check execv
         if (execv(path, args) == -1)
         {
             perror("execv");
@@ -54,19 +55,25 @@ int main(int argc, char *argv[])
         }
         else if (child_two == 0)
         {
-                // announce child two process
-                printf("\nchild process 2 pid: %d\n", getpid());
-                // child execvp
-                char *command = "ls";
-                // child two execvp call to list all files including hidden
-                char *args[] = {command, "-la", NULL};
-                if (execvp(command, args) == -1)
-                {
-                    printf("failed to execute in child process 2\n");
-                    return -1;
-                }
+            // announce child two process
+            printf("\nchild process 2 pid: %d\n", getpid());
+            // compile programn 
+            if (system("gcc -o child q4_prcs_p2.c") == -1)
+            {
+                perror("system compile error");
+                exit(EXIT_FAILURE);
+            }
+            //execute program with args using execv
+            char *path = "./child";
+            char *args[] = {"child", "source.txt","destination1.txt", "destination2.txt",NULL};
+            //error check execv
+            if (execv(path, args) == -1)
+            {
+                perror("execv");
+                exit(EXIT_FAILURE);
+            }
 
-                return 0;
+            return 0;
         }
         else
         {
